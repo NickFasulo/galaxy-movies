9abdcbc4e23755c4bd46d95b7c9fc5463236ae7f
Nicholas Fasulo <fasulo.nick@gmail.com>
2023-02-22 23:22:54 -0500

    working on layout of details page

--- src/pages/movies/[movieId].jsx
+++ src/pages/movies/[movieId].jsx
@@ -1,5 +1,5 @@
 import { useState, useEffect } from 'react'
-import { Box, Image, Stack, Badge } from '@chakra-ui/react'
+import { Box, Image, Stack, Badge, Heading, Text } from '@chakra-ui/react'
 import CustomSpinner from '../../components/CustomSpinner'
 
 export const getServerSideProps = async context => {
@@ -25,29 +25,46 @@ export default function Movie({ query }) {
     }
   }, [])
 
+  console.log(movie)
+
   return (
-    <Box>
-      {loading ? (
-        <CustomSpinner />
-      ) : (
-        <>
-          <Image
-            src={`https://image.tmdb.org/t/p/original${movie.poster_path}`}
-            fallbackSrc='fallback-1.jpg'
-            alt={movie.title}
-            objectFit='cover'
-            margin='2rem'
-            width='13rem'
-            borderRadius='1rem'
-          />
-          <Stack direction='row'>
-            {movie.genres.map((genre, i) => (
-              <Badge key={i}>{genre.name}</Badge>
-            ))}
-          </Stack>
-          <h1>{movie.title}</h1>
-        </>
-      )}
-    </Box>
+    <>
+      <Box
+        bgImage={`https://image.tmdb.org/t/p/original${movie.backdrop_path}`}
+        bgPosition='center'
+        bgRepeat='no-repeat'
+        filter='auto'
+        brightness='40%'
+        height='100vh'
+        pos='fixed'
+        zIndex={-10}
+      />
+      <Box>
+        {loading ? (
+          <CustomSpinner />
+        ) : (
+          <>
+            <Image
+              src={`https://image.tmdb.org/t/p/original${movie.poster_path}`}
+              fallbackSrc='fallback-1.jpg'
+              alt={movie.title}
+              objectFit='cover'
+              margin='2rem'
+              width='13rem'
+              borderRadius='1rem'
+              boxShadow='dark-lg'
+            />
+            <Text>{movie.tagline}</Text>
+            <Stack direction='row'>
+              {movie.genres.map((genre, i) => (
+                <Badge key={i}>{genre.name}</Badge>
+              ))}
+            </Stack>
+            <Heading>{movie.title}</Heading>
+            <Text>{movie.overview}</Text>
+          </>
+        )}
+      </Box>
+    </>
   )
 }
