21cd83b4ff0d5ed53dfe40fd4db455d15b36f0bf
Nicholas Fasulo <fasulo.nick@gmail.com>
2023-02-22 22:52:05 -0500

    add loading conditional to movie details page

--- src/pages/movies/[movieId].jsx
+++ src/pages/movies/[movieId].jsx
@@ -1,4 +1,6 @@
 import { useState, useEffect } from 'react'
+import { Box, Image, Stack, Badge } from '@chakra-ui/react'
+import CustomSpinner from '../../components/CustomSpinner'
 
 export const getServerSideProps = async context => {
   return {
@@ -9,7 +11,8 @@ export const getServerSideProps = async context => {
 }
 
 export default function Movie({ query }) {
-  const [movieInfo, setMovieInfo] = useState({})
+  const [movie, setMovie] = useState({})
+  const [loading, setLoading] = useState(true)
 
   useEffect(() => {
     return async () => {
@@ -17,13 +20,34 @@ export default function Movie({ query }) {
         `https://api.themoviedb.org/3/movie/${query.movieId}?api_key=${process.env.NEXT_PUBLIC_TMDB_KEY}`
       )
       const movieData = await res.json()
-      setMovieInfo(movieData)
+      setMovie(movieData)
+      setLoading(false)
     }
   }, [])
 
   return (
-    <div>
-      <h1>{movieInfo.title}</h1>
-    </div>
+    <Box>
+      {loading ? (
+        <CustomSpinner />
+      ) : (
+        <>
+          <Image
+            src={`https://image.tmdb.org/t/p/original${movie.poster_path}`}
+            fallbackSrc='fallback-1.jpg'
+            alt={movie.title}
+            objectFit='cover'
+            margin='2rem'
+            width='13rem'
+            borderRadius='1rem'
+          />
+          <Stack direction='row'>
+            {movie.genres.map((genre, i) => (
+              <Badge key={i}>{genre.name}</Badge>
+            ))}
+          </Stack>
+          <h1>{movie.title}</h1>
+        </>
+      )}
+    </Box>
   )
 }
