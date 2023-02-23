559c4fdf1cf0b406973fb9daa8665074e8765e3c
Nicholas Fasulo <fasulo.nick@gmail.com>
2023-02-23 14:49:58 -0500

    render background image of details page

--- src/pages/movies/[movieId].jsx
+++ src/pages/movies/[movieId].jsx
@@ -1,5 +1,5 @@
 import { useState, useEffect } from 'react'
-import { Box, Image, Stack, Badge, Heading, Text } from '@chakra-ui/react'
+import { Flex, Box, Image, Stack, Badge, Heading, Text } from '@chakra-ui/react'
 import CustomSpinner from '../../components/CustomSpinner'
 
 export const getServerSideProps = async context => {
@@ -28,43 +28,70 @@ export default function Movie({ query }) {
   console.log(movie)
 
   return (
-    <>
-      <Box
-        bgImage={`https://image.tmdb.org/t/p/original${movie.backdrop_path}`}
-        bgPosition='center'
-        bgRepeat='no-repeat'
+    <Flex position='relative' overflow='hidden' height='100vh'>
+      <Image
+        src={`https://image.tmdb.org/t/p/original${movie.backdrop_path}`}
+        position='absolute'
         filter='auto'
-        brightness='40%'
-        height='100vh'
-        pos='fixed'
-        zIndex={-10}
+        brightness='35%'
       />
-      <Box>
-        {loading ? (
-          <CustomSpinner />
-        ) : (
-          <>
+      {loading ? (
+        <CustomSpinner />
+      ) : (
+        <>
+          <Flex
+            alignItems='center'
+            flexDirection='column'
+            position='relative'
+            margin='2rem'
+          >
             <Image
               src={`https://image.tmdb.org/t/p/original${movie.poster_path}`}
               fallbackSrc='fallback-1.jpg'
               alt={movie.title}
               objectFit='cover'
-              margin='2rem'
-              width='13rem'
+              width='16rem'
               borderRadius='1rem'
               boxShadow='dark-lg'
             />
-            <Text>{movie.tagline}</Text>
-            <Stack direction='row'>
+            <Stack direction='row' margin='1rem 0'>
               {movie.genres.map((genre, i) => (
                 <Badge key={i}>{genre.name}</Badge>
               ))}
             </Stack>
-            <Heading>{movie.title}</Heading>
-            <Text>{movie.overview}</Text>
-          </>
-        )}
-      </Box>
-    </>
+            <Text
+              fontSize='lg'
+              align='center'
+              color='white'
+              textShadow='2px 0 4px black'
+            >
+              {movie.tagline}
+            </Text>
+          </Flex>
+          <Box
+            margin='2rem'
+            padding='1rem'
+            position='relative'
+            height='fit-content'
+          >
+            <Heading
+              margin='1rem 0 2rem'
+              color='white'
+              textShadow='2px 0 4px black'
+            >
+              {movie.title}
+            </Heading>
+            <Text
+              fontSize='lg'
+              width='40rem'
+              color='white'
+              textShadow='2px 0 4px black'
+            >
+              {movie.overview}
+            </Text>
+          </Box>
+        </>
+      )}
+    </Flex>
   )
 }
