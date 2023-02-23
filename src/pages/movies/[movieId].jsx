a4d5d74a66d8e2ed16d5d2628403b1708babbc76
Nicholas Fasulo <fasulo.nick@gmail.com>
2023-02-22 21:39:21 -0500

    update dynamic movie route

--- src/pages/movies/[movieId].jsx
+++ src/pages/movies/[movieId].jsx
@@ -1,39 +1,29 @@
-export const getStaticPaths = async () => {
-  const res = await fetch(
-    `https://api.themoviedb.org/3/movie/popular?api_key=${process.env.NEXT_PUBLIC_TMDB_KEY}`
-  )
-  const moviesData = await res.json()
+import { useState, useEffect } from 'react'
 
-  const paths = moviesData.results.map(movie => {
-    return {
-      params: { movieId: movie.id.toString() }
-    }
-  })
-
-  return {
-    paths,
-    fallback: false
-  }
-}
-
-export const getStaticProps = async context => {
-  const movieId = context.params.movieId
-  const res = await fetch(
-    `https://api.themoviedb.org/3/movie/${movieId}?api_key=${process.env.NEXT_PUBLIC_TMDB_KEY}`
-  )
-  const movieData = await res.json()
-
+export const getServerSideProps = async context => {
   return {
     props: {
-      movieData
+      query: context.query
     }
   }
 }
 
-export default function Movie({ movieData }) {
+export default function Movie({ query }) {
+  const [movieInfo, setMovieInfo] = useState({})
+
+  useEffect(() => {
+    return async () => {
+      const res = await fetch(
+        `https://api.themoviedb.org/3/movie/${query.movieId}?api_key=${process.env.NEXT_PUBLIC_TMDB_KEY}`
+      )
+      const movieData = await res.json()
+      setMovieInfo(movieData)
+    }
+  }, [])
+
   return (
     <div>
-      <h1>{movieData.title}</h1>
+      <h1>{movieInfo.title}</h1>
     </div>
   )
 }
