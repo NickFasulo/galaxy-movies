9aed42652020f2c910ea95f972c148677f6f7913
Inny Taylor <innytaylor@Innys-MacBook-Air.local>
2026-09-24 01:14:07 -0700

    add google adsense meta tag

--- src/pages/index.jsx
+++ src/pages/index.jsx
@@ -175,6 +175,7 @@ export default function Home() {
         <meta name='twitter:description' content={pageDescription} />
         <meta name='twitter:image' content={ogImage} />
         <meta name='impact-site-verification' value='1ad17b76-2079-4639-9f17-f65f40948c6b' />
+        <meta name="google-adsense-account" content="ca-pub-7970999589560353" />
 
         <script
           type='application/ld+json'
