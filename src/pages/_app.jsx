27e5a713084f7ffe34af51a906e0046a5783e7b1
Inny Taylor <innytaylor@Innys-MacBook-Air.local>
2026-09-23 20:53:30 -0700

    add tiun

--- src/pages/_app.jsx
+++ src/pages/_app.jsx
@@ -1,14 +1,22 @@
-import { useMemo } from 'react'
+import { useMemo, useEffect } from 'react'
 import { Hydrate, QueryClient, QueryClientProvider } from 'react-query'
 import { ChakraProvider } from '@chakra-ui/react'
 import { Analytics } from '@vercel/analytics/react'
 import { useScrollRestore } from '../hooks/useScrollRestore'
+import { tiun } from '@tiun/sdk'
 import '../styles/globals.css'
 
 function MyApp({ Component, pageProps }) {
   const queryClient = useMemo(() => new QueryClient(), [])
 
   useScrollRestore()
+
+  useEffect(() => {
+    tiun.init({
+      snippetId: 'YOUR_SNIPPET_ID',
+      language: 'en'
+    })
+  }, [])
 
   return (
     <QueryClientProvider client={queryClient}>
