791607dbee4611df0a005117c6fdb31864f565c5
Inny Taylor <innytaylor@Innys-MacBook-Air.local>
2026-09-26 09:07:31 -0700

    edit badge

--- src/pages/about.jsx
+++ src/pages/about.jsx
@@ -1,12 +1,15 @@
 import InfoPage, { InfoParagraph } from '../components/InfoPage'
 import BackButton from '../components/BackButton'
+import { Box } from '@chakra-ui/react'
 
 export default function About() {
   return (
     <InfoPage title='About Galaxy Movies' description='Learn about Galaxy Movies and its movie discovery features.'>
       <InfoParagraph>Galaxy Movies helps people discover popular, new, and upcoming movies in one place.</InfoParagraph>
       <InfoParagraph>Movie information and availability data are supplied by The Movie Database. Availability may vary by country and can change over time.</InfoParagraph>
-      <a href="https://launchaf.com/" target="_blank" rel="noopener" data-launchaf-badge="true"><img src="https://launchaf.com/api/badge/light?v=launchaf-blue-2026-2" alt="Featured on LaunchAF" width="200" height="56" style={{ marginBottom: '1.5rem' }} /></a>
+      <Box mb="1.5rem">
+        <a href="https://launchaf.com/" target="_blank" rel="noopener" data-launchaf-badge="true"><img src="https://launchaf.com/api/badge/light?v=launchaf-blue-2026-2" alt="Featured on LaunchAF" width="200" height="56" /></a>
+      </Box>
       <BackButton />
     </InfoPage>
   )
